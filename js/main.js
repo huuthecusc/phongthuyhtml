245c832fef7e0e1e720536729a3734891abfbd63
The Tran <the.tran@pninformatics.com>
2020-12-05 21:22:10 +0700

    new

---
+++ js/main.js
@@ -0,0 +1,7 @@
+/* ------------------- Menu ------------------- */
+function Menu(){
+    $('.menu .menulist').attr('style','display:block');
+}
+function MenuClose(){
+    $('.menu .menulist').attr('style','display:none');
+}
