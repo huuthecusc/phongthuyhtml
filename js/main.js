95d7c11e4b36aa3cefee1cd25dbdacff87b57bb1
The Tran <the.tran@pninformatics.com>
2021-03-05 22:26:05 +0700

    add Que Luc Hao

--- js/main.js
+++ js/main.js
@@ -1,3 +1,16 @@
+$(document).ready(function(){
+    $('.mtabs .tabnav').click(function(){
+        if($(this).hasClass('tab1')){
+            $('.mtabs .ctab').hide();
+            $('.mtabs .ctab1').slideDown('slow');
+        }
+        else if($(this).hasClass('tab2')){
+            $('.mtabs .ctab').hide();
+            $('.mtabs .ctab2').slideDown('slow');
+        }
+    });
+});
+
 /* ------------------- Menu ------------------- */
 function Menu(){
     $('.menu .menulist').attr('style','display:block');
